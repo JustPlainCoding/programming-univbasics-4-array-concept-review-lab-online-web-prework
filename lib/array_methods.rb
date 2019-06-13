b7b4869d3de774199cbd47739834da2be839c5e8
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-13 21:24:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  
+  array.each_with_index do |element, index|
+    if element == value_to_find
+      return index
+    end
+  end
 end
 
 def find_max_value(array)
